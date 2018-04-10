d5ca914162a617218feceee41e4439b7b071575f
DESKTOP-OAK\mi <michal.sflap@gmail.com>
2018-04-10 05:09:28 +0200

    wiele kontrolek, poprawka na obsługę wielu instancji

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import MovieTable, { MovieTableControll, MovieTableManager } from "./components/MovieTable";
-import ReactObserver from 'react-event-observer';
+import MovieTable, { MovieTableControll } from "./components/MovieTable";
+import ReactObserver from "react-event-observer";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.observer = ReactObserver()
+        this.observer = ReactObserver();
     }
-    
+
     componentDidMount() {
-        this.tableControl = ReactObserver()
-        this.listener = this.observer.subscribe('table_description_changed', data => {
-            console.log('table_description_changed', data)
-        })
-        console.log('Listener subscribed.')
+        this.tableControl = ReactObserver();
+        this.listener = this.observer.subscribe(
+            "tab1table_description_changed",
+            data => {
+                console.log("table_description_changed", data);
+            }
+        );
+        console.log("Listener subscribed.");
     }
 
     render() {
@@ -30,9 +33,31 @@ class App extends Component {
                     reload.
                 </p>
                 <div>
-                    <MovieTableControll tableControlPublish={this.observer.publish}/>
+                <MovieTableControll
+                        tableControlPublish={this.observer.publish}
+                        tableControlName="tab1"
+                    />
+                    <MovieTableControll
+                        tableControlPublish={this.observer.publish}
+                        tableControlName="tab1"
+                    />
                     <div className="container">
-                        <MovieTable tableControlSubscribe={this.observer.subscribe}/>
+                        <MovieTable
+                            tableControlSubscribe={this.observer.subscribe}
+                            tableControlName="tab1"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <MovieTableControll
+                        tableControlPublish={this.observer.publish}
+                        tableControlName="tab2"
+                    />
+                    <div className="container">
+                        <MovieTable
+                            tableControlSubscribe={this.observer.subscribe}
+                            tableControlName="tab2"
+                        />
                     </div>
                 </div>
             </div>
--- src/components/MovieTable.jsx
+++ src/components/MovieTable.jsx
@@ -1,82 +1,118 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 //import 'bootstrap' from 'react-bootstrap'
-import { Glyphicon, Form, FormGroup, Checkbox, Label } from 'react-bootstrap'
-import MicroEvent from './../microevent'
+import { Glyphicon, Form, FormGroup, Checkbox } from "react-bootstrap";
+import MicroEvent from "./../microevent";
+
+/*
+
+This solutions allows have many controls of the exact one table, but requires to manually
+manage proper name propagation.
+Also it requires to parent component to provide observer methods to nested components.
+Hence, the state of the control is not synchronized between them and can cause strange results.
+
+@todo pobawić się stanami (Redux)
+@todo wprowadzić podwójnych obserwatorów, jeden na stan tablicy, drugi na stan kontrolki
+    ewentualnie zmienić przekazywanie funkcji na przekazywanie observera.
+
+    A może w obóle zrobić przekazywanie 'stanu' w obiekcie "data" eventu observera
+
+*/
 
 class MovieType extends React.Component {
     render() {
         const type_resolve = {
-            'movie': 'film',
-            'tvSeries': 'retweet',
-            'tvShort': 'eye-close',
-            'short': 'eye-close',
-            'tvEpisode': 'check',
-            'video': 'facetime-video',
-        }
-        let icon = (type_resolve[this.props.titleType]) ? type_resolve[this.props.titleType] : 'arrow-down'
-        return <Glyphicon glyph={icon} />
+            movie: "film",
+            tvSeries: "retweet",
+            tvShort: "eye-close",
+            short: "eye-close",
+            tvEpisode: "check",
+            video: "facetime-video"
+        };
+        let icon = type_resolve[this.props.titleType]
+            ? type_resolve[this.props.titleType]
+            : "arrow-down";
+        return <Glyphicon glyph={icon} />;
     }
 }
 
-var Ticker = function(){
+MovieType.propTypes = {
+    titleType: PropTypes.string
+};
+
+var Ticker = function() {
     var self = this;
-    setInterval(function(){
-        self.trigger('tick', new Date());
-    }, 1000);
+    setInterval(function() {
+        self.trigger("tick", new Date());
+    }, 5000);
 };
 
 export class MovieTableControll extends React.Component {
     constructor(props) {
-        super(props)
-        console.log('Constructor od MTC', this.props.tableControlPublish)
+        super(props);
+        console.log("Constructor od MTC", this.props.tableControlPublish);
     }
 
     componentDidMount() {
-        MicroEvent.mixin(Ticker)
-        this.ticker = new Ticker()
-        this.ticker.bind('tick', date => {
-            console.log('notified date', date)
-        })
+        MicroEvent.mixin(Ticker);
+        this.ticker = new Ticker();
+        this.ticker.bind("tick", date => {
+            console.log("notified date", date);
+        });
     }
 
-    handleChange = (e) => {
-        console.log('Changed')
-        this.props.tableControlPublish('table_description_changed', 1)
-    }
+    handleChange = () => {
+        console.log("Changed");
+        this.props.tableControlPublish(
+            `${this.props.tableControlName}table_description_changed`,
+            1
+        );
+    };
 
     render() {
-        return <form action="none">
+        return (
+            <Form>
                 <FormGroup>
-                    <Checkbox onChange={this.handleChange}>Show descriptions</Checkbox>
+                    <Checkbox onChange={this.handleChange}>
+                        Show descriptions
+                    </Checkbox>
                 </FormGroup>
-            </form>
+            </Form>
+        );
     }
 }
 
+MovieTableControll.propTypes = {
+    tableControlName: PropTypes.string.isRequired,
+    tableControlPublish: PropTypes.func
+};
+
 class MovieTable extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            descriptions: false,
-        }
+            descriptions: false
+        };
     }
 
     componentDidMount() {
-        this.listener = this.props.tableControlSubscribe('table_description_changed', data => {
-            this.toggle_descriptions(this.state)
-            console.log('Listener WORKS!')
-        })
+        this.listener = this.props.tableControlSubscribe(
+            `${this.props.tableControlName}table_description_changed`,
+            data => {
+                this.toggle_descriptions(this.state);
+                console.log("Listener WORKS!", data);
+            }
+        );
     }
-    
-      
-    toggle_descriptions(prevState) {
-        this.setState({
-            descriptions: !prevState.descriptions,
-        })
+
+    toggle_descriptions() {
+        this.setState(prevState => ({
+            descriptions: !prevState.descriptions
+        }));
     }
 
     render() {
-        const data_file = require('./../movies.json')
+        const data_file = require("./../movies.json");
         const headers = [
             "titleType",
             "primaryTitle",
@@ -85,22 +121,22 @@ class MovieTable extends React.Component {
             "startYear",
             "endYear",
             "runtimeMinutes",
-            "genres",
-        ].map(h => <th key={h}>{h}</th>)
-
+            "genres"
+        ].map(h => <th key={h}>{h}</th>);
 
         return (
             <div className="table-responsive">
                 <table className="table table-small-font table-bordered table-striped">
                     <thead>
-                        <tr>
-                           {headers}
-                        </tr>
+                        <tr>{headers}</tr>
                     </thead>
                     <tbody>
                         {data_file.map(movie => (
                             <tr key={movie.tconst}>
-                                <td><MovieType titleType={movie.titleType} />{this.state.descriptions && movie.titleType}</td>
+                                <td>
+                                    <MovieType titleType={movie.titleType} />
+                                    {this.state.descriptions && movie.titleType}
+                                </td>
                                 <td>{movie.primaryTitle}</td>
                                 <td>{movie.originalTitle}</td>
                                 <td>{movie.isAdult}</td>
@@ -117,4 +153,9 @@ class MovieTable extends React.Component {
     }
 }
 
-export default MovieTable
+MovieTable.propTypes = {
+    tableControlName: PropTypes.string.isRequired,
+    tableControlSubscribe: PropTypes.func
+};
+
+export default MovieTable;
